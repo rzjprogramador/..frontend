e8fb6ed43279006ac74f9cb3c6b8e9702bd1c5cb
rzjprogramador <rzjprogramador@gmail.com>
2023-03-06 16:01:40 -0300

    feat: variaveis em dart

---
+++ ..dart/DartBase/01.variaveis/index.dart
@@ -0,0 +1,44 @@
+void main() {
+  // bool verdadeiro = true;
+  // print(verdadeiro);
+
+// num qualquerNumero = 10.99;
+  // print(qualquerNumero);
+
+// int numeroInteiro = 10;
+  // print(numeroInteiro);
+
+// double numeroDecimal = 10.77;
+  // print(numeroDecimal);
+
+// String? valorZero_Texto;
+  // print(valorZero_Texto);
+
+// String pularLinhaUniversal = "linha1\nLinha2\nlinha3";
+  // print(pularLinhaUniversal);
+
+// String pularLinhaEmDart = "linha1 \n" "linha2 \n" "linha3 \n";
+  // print(pularLinhaEmDart);
+
+  String varFoo = "Foo";
+  // String Interpolar_Variavel_Em_Texto_Universal = "O valor da variavel interpolada é >> " + varFoo;
+
+  // String Interpolar_Variavel_Em_Texto_Dart = "O valor é $varFoo";
+
+  /* OBJETO *************************************************** */
+  var nome = 'rei';
+  var numero = 10;
+  Object objFoo = {nome, numero};
+
+  // String Interpolar_ObjetoFuncao_Em_Texto_Universal = "O valor é >> "+objFoo;
+
+  String Interpolar_ObjetoFuncao_Em_Texto_Dart =
+      "o valor do objeto é >> ${objFoo}";
+
+  // print(Interpolar_Variavel_Em_Texto_Universal);
+  // print(Interpolar_Variavel_Em_Texto_Dart);
+  // print(Interpolar_ObjetoFuncao_Em_Texto_Universal);
+  print(Interpolar_ObjetoFuncao_Em_Texto_Dart);
+
+  /* *************************************************** */
+}
